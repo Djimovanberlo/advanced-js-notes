336010bd73bde5c49b4ad477f655f63f4e285f16
Djimo van Berlo <djimovanberlo@gmail.com>
2022-01-25 16:12:58 +0100

    uncommented c.log()

--- passed-by-ref-val.js
+++ passed-by-ref-val.js
@@ -19,7 +19,7 @@ let obj2 = obj1 // this creates a REFERENCE to obj1, no values are copied
 
 obj2.title = 'Lord of Utrecht'
 
-// console.log('1: ', obj1, '2: ', obj2) // expected outcome: 2x { name: 'Djimo', title: 'Lord of Utrecht' }
+console.log('1: ', obj1, '2: ', obj2) // expected outcome: 2x { name: 'Djimo', title: 'Lord of Utrecht' }
 
 // This way, you're not claiming much memory when using objects and arrays. Isn't that nice!
 
@@ -30,14 +30,14 @@ const firstArray = [1, 2, 3]
 const secondArray = [...firstArray]
 secondArray.push(4)
 
-// console.log('1: ', firstArray, '2: ', secondArray) // expected outcome: [1, 2, 3], [1, 2, 3, 4]
+console.log('1: ', firstArray, '2: ', secondArray) // expected outcome: [1, 2, 3], [1, 2, 3, 4]
 
 // This does the same thing as a spread operator
 const firstObj = { greeting: 'Hi' }
 const secondObj = Object.assign({}, firstObj)
 secondObj.greeting = 'Hello'
 
-// console.log('1: ', firstObj, '2: ', secondObj)
+console.log('1: ', firstObj, '2: ', secondObj)
 
 // -----------
 
@@ -55,7 +55,7 @@ const spreadNestedObj = { ...nestedObj }
 
 spreadNestedObj.titles.first = 'Lord of LLN'
 
-// console.log('1: ', nestedObj, '2: ', spreadNestedObj) // expected outcome: both objects contain Lord of LLN
+console.log('1: ', nestedObj, '2: ', spreadNestedObj) // expected outcome: both objects contain Lord of LLN
 
 // So the nested object is still passed by reference! Oh no
 
@@ -70,7 +70,7 @@ console.log('1: ', nestedObj, '2: ', anotherSpread) // expected result: two obje
 const veryCloned = JSON.parse(JSON.stringify(nestedObj))
 veryCloned.titles.first = 'Lord of the univerise, I guess...'
 
-// console.log('1: ', nestedObj, '2: ', veryCloned) // expected result: two objects with different values
+console.log('1: ', nestedObj, '2: ', veryCloned) // expected result: two objects with different values
 
 // This makes a stringified JSON out of the object, and then parses the JSON to create a new object
 // If it were to be a huuuge object, this will result in performance issues
